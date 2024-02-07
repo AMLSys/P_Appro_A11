ced8d336fee4897ea73e545cfa3587731962c0f7
Aurélien Maxence Lahaye <po79tvx@eduvaud.ch>
2024-02-07 10:07:50 +0100

    Creation of the navbar

---
+++ resources/siteparts/nav.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <!--
+		    Author: Aurélien Lahaye
+		    Date: 05.02.2024
+		    Description: Nav of the webiste
+	    -->
+        <title>Bouldero - Nav</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../resources/css/nav.css">
+        <link rel="icon" type="image/png" href="../../resources/icones/Bouldero_Logo.svg">
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+        </style>
+    </head>
+    <nav>
+    <div class="flex-nav">
+        <div class="collored-line"></div>
+            <div class="nav-look">
+                <div class="nav-space">
+                    <div class="item">Chaussons</div>
+                    <div class="item">Sac à Pof</div>
+                    <div class="item">Vêtements</div>
+                    <div class="item">Entraînement</div>
+                    <div class="item">Accessoires</div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</html>
--- src/html/homepage.php
+++ src/html/homepage.php
@@ -6,13 +6,12 @@
 		    Date: 05.02.2024
 		    Description: Header of the webiste
 	    -->
-        <title>Home page</title>
+        <title>Bouldero - Home page</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../../resources/css/shared.css">
         <link rel="stylesheet" href=".../resources/css/header.css">
     </head>
-    <body>
-        <?php require('../../resources/siteparts/header.php'); ?>
-    </body>
+    <header><?php require('../../resources/siteparts/header.php'); ?></header>
+    <nav><?php require('../../resources/siteparts/nav.php'); ?></nav>
 </html>
